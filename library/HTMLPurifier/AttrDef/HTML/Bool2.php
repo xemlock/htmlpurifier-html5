a9e92eabb4f3ed3703d2cb356b74d5ef8b2d28b4
xemlock <xemlock@gmail.com>
2015-08-10 10:16:20 +0200

    updated HTML Purifier to 4.7.*

--- library/HTMLPurifier/AttrDef/HTML/Bool2.php
+++ library/HTMLPurifier/AttrDef/HTML/Bool2.php
@@ -6,8 +6,8 @@
  * HTMLPurifier 4.6.0 has broken support for boolean attributes, as reported
  * in: http://htmlpurifier.org/phorum/read.php?3,7631,7631
 
- * This issue has (almost) been fixed in ezyang/htmlpurifier@c67e4c2f7e06f89c,
- * but so far it hasn't been merged into a stable version.
+ * This issue has almost been fixed in 4.7.0, boolean attributes are properly
+ * parsed, but their values are not validated.
  */
 class HTMLPurifier_AttrDef_HTML_Bool2 extends HTMLPurifier_AttrDef_HTML_Bool
 {

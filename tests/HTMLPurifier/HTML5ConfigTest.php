1c747b9df4e1403829b7b0834fbb454542de61d7
xemlock <xemlock@gmail.com>
2018-12-10 11:26:24 +0100

    Add test for loading config from INI file

--- tests/HTMLPurifier/HTML5ConfigTest.php
+++ tests/HTMLPurifier/HTML5ConfigTest.php
@@ -9,17 +9,30 @@ class HTMLPurifier_HTML5ConfigTest extends PHPUnit_Framework_TestCase
 
         $config2 = HTMLPurifier_HTML5Config::create($config);
         $this->assertSame($config->def, $config2->def);
+    }
 
-        $config3 = HTMLPurifier_HTML5Config::create(array(
+    public function testCreateFromArray()
+    {
+        $config = HTMLPurifier_HTML5Config::create(array(
             'Core.Encoding' => 'iso-8859-1',
         ));
-        $this->assertInstanceOf('HTMLPurifier_Config', $config3);
-        $this->assertEquals('iso-8859-1', $config3->get('Core.Encoding'));
+        $this->assertInstanceOf('HTMLPurifier_Config', $config);
+        $this->assertEquals('iso-8859-1', $config->get('Core.Encoding'));
+    }
 
-        $config4 = HTMLPurifier_HTML5Config::create(null);
-        $config4->set('Core.Encoding', 'iso-8859-1');
-        $this->assertInstanceOf('HTMLPurifier_Config', $config4);
-        $this->assertEquals('iso-8859-1', $config4->get('Core.Encoding'));
+    public function testCreateFromNull()
+    {
+        $config = HTMLPurifier_HTML5Config::create(null);
+        $config->set('Core.Encoding', 'iso-8859-1');
+        $this->assertInstanceOf('HTMLPurifier_Config', $config);
+        $this->assertEquals('iso-8859-1', $config->get('Core.Encoding'));
+    }
+
+    public function testCreateFromIni()
+    {
+        $config = HTMLPurifier_HTML5Config::create(dirname(__FILE__) . '/../assets/ConfigTest.ini');
+        $this->assertInstanceOf('HTMLPurifier_Config', $config);
+        $this->assertEquals('iso-8859-1', $config->get('Core.Encoding'));
     }
 
     public function testInherit()

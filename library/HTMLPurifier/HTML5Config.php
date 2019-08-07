047bee025dfbff96181951fadfbfa6fd9309919a
xemlock <xemlock@gmail.com>
2019-08-07 11:52:14 +0200

    Bump REVISION

--- library/HTMLPurifier/HTML5Config.php
+++ library/HTMLPurifier/HTML5Config.php
@@ -2,7 +2,7 @@
 
 class HTMLPurifier_HTML5Config extends HTMLPurifier_Config
 {
-    const REVISION = 2019080601;
+    const REVISION = 2019080701;
 
     /**
      * @param  string|array|HTMLPurifier_Config $config

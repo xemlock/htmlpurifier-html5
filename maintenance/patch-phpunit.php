f4738d25a85d7a85d82cb958dd88610036e18c3b
xemlock <xemlock@gmail.com>
2021-01-04 11:56:54 +0100

    Patch TestCase.php so that tests run in more recent phpunit versions

---
+++ maintenance/patch-phpunit.php
@@ -0,0 +1,21 @@
+<?php
+
+$ROOT_DIR = realpath(__DIR__ . '/..');
+$TESTCASE_RELPATH = 'vendor/phpunit/phpunit/src/Framework/TestCase.php';
+$TESTCASE_PATH = $ROOT_DIR . '/' . $TESTCASE_RELPATH;
+
+if (file_exists($TESTCASE_PATH)) {
+    // void type annotation in setUp() and tearDown() effectively prevents tests
+    // to be valid in phpunit 8.0
+    $contents = file_get_contents($TESTCASE_PATH);
+    $patchedContents = preg_replace('/protected function (setUp|tearDown)\(\): void/', 'protected function $1()', $contents);
+
+    if ($contents !== $patchedContents) {
+        file_put_contents($TESTCASE_PATH, $patchedContents);
+        echo "Patched file ${TESTCASE_RELPATH}\n";
+    } else {
+        echo "No changes made\n";
+    }
+} else {
+    echo "File not found: ${TESTCASE_RELPATH}\n";
+}

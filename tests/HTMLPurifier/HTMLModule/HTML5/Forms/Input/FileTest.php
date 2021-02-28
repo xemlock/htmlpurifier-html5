21536917455690b9dfaf5f6391263b95f8d8016b
xemlock <xemlock@gmail.com>
2021-02-28 16:06:09 +0100

    Add tests for input type=hidden

--- tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/FileTest.php
+++ tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/FileTest.php
@@ -10,7 +10,7 @@ class HTMLPurifier_HTMLModule_HTML5_Forms_Input_FileTest
                 '<input type="file" name="uploads" accept=".jpg, .jpeg, .png, .svg, .gif" multiple required>',
             ),
             'input file invalid attributes' => array(
-                '<input type="file" alt="foo" checked dirname="foo.dir" height="10" max="10" maxlength="10" min="0" minlength="0" pattern="" size="10" src="foo.png" step="1" value="foo.jpg" width="10">',
+                '<input type="file" alt="foo" checked dirname="foo.dir" height="10" max="10" maxlength="10" min="0" minlength="0" pattern="[a-z]+" size="10" src="foo.png" step="1" value="foo.jpg" width="10">',
                 '<input type="file">',
             ),
         );
--- tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/HiddenTest.php
+++ tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/HiddenTest.php
@@ -3,4 +3,23 @@
 class HTMLPurifier_HTMLModule_HTML5_Forms_Input_HiddenTest
     extends HTMLPurifier_HTMLModule_HTML5_Forms_InputTest
 {
+    public function dataProvider()
+    {
+        return array(
+            'input hidden' => array(
+                '<input type="hidden" name="foo" value="foo" disabled>',
+            ),
+            'input hidden no value attribute' => array(
+                '<input type="hidden">',
+                '<input type="hidden" value="">',
+            ),
+            'input hidden empty value attribute' => array(
+                '<input type="hidden" value="">',
+            ),
+            'input hidden invalid attributes' => array(
+                '<input type="hidden" accept="text/plain" alt="foo" checked dirname="foo.dir" height="10" max="10" maxlength="64" min="0" minlength="0" multiple pattern="[a-z]+" placeholder="foo" readonly required size="10" src="foo.png" step="1" width="10">',
+                '<input type="hidden" value="">',
+            ),
+        );
+    }
 }

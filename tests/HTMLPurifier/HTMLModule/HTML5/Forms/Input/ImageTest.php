49116d05d883aebe1f982c9340de58089f02e474
xemlock <xemlock@gmail.com>
2021-03-02 08:18:55 +0100

    Update image and month tests

--- tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/ImageTest.php
+++ tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/ImageTest.php
@@ -6,6 +6,9 @@ class HTMLPurifier_HTMLModule_HTML5_Forms_Input_ImageTest
     public function dataProvider()
     {
         return array(
+            'input image' => array(
+                '<input type="image" alt="foo" height="10" src="http://foo.com/foo.png" width="10">',
+            ),
             'input image alt default' => array(
                 '<input type="image">',
                 '<input type="image" alt="image">',
@@ -13,6 +16,10 @@ class HTMLPurifier_HTMLModule_HTML5_Forms_Input_ImageTest
             'input image alt from name' => array(
                 '<input type="image" name="image1">',
                 '<input type="image" name="image1" alt="image1">',
+            ),
+            'input image invalid attributes' => array(
+                '<input type="image" accept="image/jpeg" alt="foo" checked dirname="foo.dir" height="10" max="10" maxlength="64" min="0" minlength="0" multiple pattern="[a-z]+" placeholder="foo" readonly required size="10" src="foo.png" step="1" value="foo.png" width="10">',
+                '<input type="image" alt="foo" height="10" src="foo.png" width="10">',
             ),
         );
     }
--- tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/MonthTest.php
+++ tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/MonthTest.php
@@ -6,12 +6,18 @@ class HTMLPurifier_HTMLModule_HTML5_Forms_Input_MonthTest
     public function dataProvider()
     {
         return array(
-            'input month valid min max attributes' => array(
-                '<input type="month" min="2005-04" max="2010-04" step="1">',
+            'input month valid attributes' => array(
+                '<input type="month" min="2005-04" max="2010-04" step="1" value="2009-01">',
             ),
-            'input month invalid min max attributes' => array(
-                '<input type="month" min="2005" max="2010" step="1">',
-                '<input type="month" step="1">',
+            'input month empty value' => array(
+                '<input type="month" value="">',
+            ),
+            'input month no value' => array(
+                '<input type="month">',
+            ),
+            'input month invalid attributes' => array(
+                '<input type="month" accept="text/plain" alt="foo" checked dirname="foo.dir" height="10" max="10" maxlength="64" min="0" minlength="0" multiple pattern="[a-z]+" placeholder="foo" readonly required size="10" src="foo.png" step="1" value="foo" width="10">',
+                '<input type="month" readonly required step="1">',
             ),
         );
     }

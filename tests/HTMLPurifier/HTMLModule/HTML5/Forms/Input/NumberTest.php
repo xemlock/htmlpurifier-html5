807473594740bbd70c5f6033c44e468137118f77
xemlock <xemlock@gmail.com>
2021-03-04 11:01:33 +0100

    Add tests for type=password

--- tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/NumberTest.php
+++ tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/NumberTest.php
@@ -7,12 +7,16 @@ class HTMLPurifier_HTMLModule_HTML5_Forms_Input_NumberTest
     {
         return array(
             'input number valid attributes' => array(
-                '<input type="number" min="10" max="100" step=".01" value="21.37">',
+                '<input type="number" disabled min="10" max="100" step=".01" value="21.37" placeholder="foo" readonly required>',
             ),
             'input number empty value' => array(
                 '<input type="number" value="">',
             ),
             'input number no value' => array(
+                '<input type="number">',
+            ),
+            'input number invalid value' => array(
+                '<input type="number" value="foo">',
                 '<input type="number">',
             ),
             'input number invalid attributes' => array(
@@ -21,8 +25,8 @@ class HTMLPurifier_HTMLModule_HTML5_Forms_Input_NumberTest
                 // element: accept, alt, checked, dirname, formaction, formenctype, formmethod,
                 // formnovalidate, formtarget, height, maxlength, minlength, multiple, pattern,
                 // size, src, and width.
-                '<input type="number" accept="text/plain" alt="foo" checked dirname="foo.dir" disabled height="10" max="10" maxlength="64" min="0" minlength="0" multiple pattern="[a-z]+" placeholder="foo" readonly required size="10" src="foo.png" step="1" value="foo" width="10">',
-                '<input type="number" disabled max="10" min="0" placeholder="foo" readonly required step="1">',
+                '<input type="number" accept="text/plain" alt="foo" checked dirname="foo.dir" formaction="http://foo.com" formenctype="application/x-www-form-urlencoded" formmethod="post" formnovalidate formtarget="_self" height="10" maxlength="64" minlength="0" multiple pattern="[a-z]+" size="10" src="foo.png" width="10">',
+                '<input type="number">',
             ),
         );
     }
--- tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/PasswordTest.php
+++ tests/HTMLPurifier/HTMLModule/HTML5/Forms/Input/PasswordTest.php
@@ -3,4 +3,26 @@
 class HTMLPurifier_HTMLModule_HTML5_Forms_Input_PasswordTest
     extends HTMLPurifier_HTMLModule_HTML5_Forms_InputTest
 {
+    public function dataProvider()
+    {
+        return array(
+            'input password' => array(
+                '<input type="password" disabled maxlength="64" minlength="0" name="foo" pattern="[-0-9]+" placeholder="foo" readonly required value="foo" size="10">',
+            ),
+            'input password empty value' => array(
+                '<input type="password" value="">',
+            ),
+            'input password no value' => array(
+                '<input type="password">',
+            ),
+            'input password invalid attributes' => array(
+                // https://html.spec.whatwg.org/multipage/input.html#password-state-(type=password)
+                // The following content attributes must not be specified and do not apply to
+                // the element: accept, alt, checked, dirname, formaction, formenctype, formmethod,
+                // formnovalidate, formtarget, height, list, max, min, multiple, src, step, and width.
+                '<input type="password" accept="text/plain" alt="foo" checked dirname="foo.dir" formaction="http://foo.com" formenctype="application/x-www-form-urlencoded" formmethod="post" formnovalidate formtarget="_self" height="10" list="foo" max="10" min="0" multiple src="foo.png" step="1" width="10">',
+                '<input type="password">',
+            ),
+        );
+    }
 }

8b760ccfdffb1a609b7bf7497ec9fd0ef41f3fc0
Kieran Brahney <kieran@supportpal.com>
2025-02-05 14:56:50 +0000

    PHP 8.4 support

--- library/HTMLPurifier/HTML5Config.php
+++ library/HTMLPurifier/HTML5Config.php
@@ -60,9 +60,9 @@ class HTMLPurifier_HTML5Config extends HTMLPurifier_Config
 
     /**
      * @param HTMLPurifier_ConfigSchema $schema
-     * @param HTMLPurifier_PropertyList $parent OPTIONAL
+     * @param HTMLPurifier_PropertyList|null $parent OPTIONAL
      */
-    public function __construct(HTMLPurifier_ConfigSchema $schema, HTMLPurifier_PropertyList $parent = null)
+    public function __construct(HTMLPurifier_ConfigSchema $schema, $parent = null)
     {
         // ensure 'HTML5' is among allowed 'HTML.Doctype' values
         $doctypeConfig = $schema->info['HTML.Doctype'];
